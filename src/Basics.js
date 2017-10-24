6abcc703f997e07e7e265b2210c1bca7435abc59
Ben Healey <bentastic27@gmail.com>
2017-10-23 23:59:13 -0400

    Added simple props usage to the Basics.js file as well as a small margin change in css

--- src/Basics.js
+++ src/Basics.js
@@ -7,6 +7,10 @@ export class Basics extends React.Component {
     e.target.setAttribute('src', "https://i.imgur.com/zkPS0Sn.png");
   }
 
+  someOtherHandler() {
+    alert("This handler was handled");
+  }
+
   render() {
     // for basic variable exampls
     const kittyImage = "https://i.imgur.com/xdmtIh2.jpg";
@@ -52,9 +56,61 @@ export class Basics extends React.Component {
         {true && <p>Showing because of a true && thing</p>}
 
         <h2>Basic Event Handler</h2>
+        <h3>Simple Image Change</h3>
         <p>Click on the burger to turn it into a hotdog</p>
         <img src={burgerImage} alt={randomText} onClick={this.turnImageIntoHotdog} />
+        <h3>As a Prop</h3>
+        <PassMeAHandlerProp onClick={this.someOtherHandler} />
+
+        <h2>Using Props</h2>
+        <h3>Defaults With this.defaultProps</h3>
+        <PassMeProps />
+        <h3>Passing Props</h3>
+        <PassMeProps
+          name="A Fancy Name"
+          text="Some text that isn't Lorem Ipsum."
+        />
+        <h3>Props Children</h3>
+        <ListPropsChildren>
+          <li>Child One</li>
+          <li>Child Two</li>
+        </ListPropsChildren>
       </div>
+    );
+  }
+}
+
+class PassMeAHandlerProp extends React.Component {
+  render() {
+    return (
+      <p onClick={this.props.onClick} >
+        Click me to handle a click
+      </p>
+    );
+  }
+}
+
+class PassMeProps extends React.Component {
+  render() {
+    return (
+      <p>
+        <b>Name:</b> {this.props.name}<br />
+        <b>Text:</b> {this.props.text}
+      </p>
+    );
+  }
+}
+PassMeProps.defaultProps = {
+  name: "Default Name",
+  text: "Some default text. Maybe some Lorem Ipsum stuff.",
+};
+
+class ListPropsChildren extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.children}
+      </ul>
     );
   }
 }
